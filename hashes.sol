30eece8eeaf56125befa30f02e619e7258797380
hemapallevada <57134197+hemapallevada@users.noreply.github.com>
2020-09-04 19:36:57 +0530

    Update hashes.sol
    added a function to retrieve the length of hashes array

--- hashes.sol
+++ hashes.sol
@@ -13,6 +13,10 @@ contract hashes{
         allHashes[_hash]=hash(msg.sender,_hash,false);
         
     }  
+    
+     function getallHashesLength() public view returns (uint){
+        return availablehashes.length;
+    }
     function verified(string memory _hash) public{
         require(msg.sender==0xFe4Ae9EE6ff6ef78d895B2ca3F971dCa47e693A5 || msg.sender==0xfe63Cae2841EE6C2b3BF05A2EeCF1aB619C88cfc,"Sorry you ar not verifier");
         allHashes[_hash].verified=true;
